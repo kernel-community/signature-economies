f4910571987d2adb4e88f3d6b717d29a6a5013ff
Angela Gilhotra <angelagilhotra@gmail.com>
2022-05-14 15:24:20 -0300

    fix paths

--- packages/dapp/src/index.js
+++ packages/dapp/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {Provider} from 'wagmi';
-import {connectors, provider, connectorStorageKey} from './web3/connect';
+import {connectors, provider, connectorStorageKey} from './utils/connect';
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
