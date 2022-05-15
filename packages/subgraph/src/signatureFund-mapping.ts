0a56eba0ccb10395ca257e3c4a88cc88db316d80
andytudhope <tuddy0525@gmail.com>
2022-05-15 11:29:25 +0200

    subgraph fixes

--- packages/subgraph/src/signatureFund-mapping.ts
+++ packages/subgraph/src/signatureFund-mapping.ts
@@ -2,7 +2,7 @@ import { BigInt, log } from '@graphprotocol/graph-ts';
 import {
   Approval,
   ApprovalForAll,
-  DonationReceived,
+  SignCreated,
   Transfer,
 } from '../generated/SignatureFund/SignatureFund';
 import { Account, SignatureFund } from '../generated/schema';
@@ -12,22 +12,22 @@ export function handleApproval(event: Approval): void {}
 
 export function handleApprovalForAll(event: ApprovalForAll): void {}
 
-export function handleDonationReceived(event: DonationReceived): void {
-  const donor = event.params.donor.toHexString();
+export function handleSignCreated(event: SignCreated): void {
+  const signer = event.params.signer.toHexString();
   const tokenId = event.params.tokenId.toString();
 
-  const donorAccount: Account = getOrCreateAccount(donor);
+  const signerAccount: Account = getOrCreateAccount(signer);
 
   let nft = SignatureFund.load(tokenId);
   if (nft == null) {
     nft = new SignatureFund(tokenId);
   }
-  nft.steward = donorAccount.id;
-  nft.donationAmount = event.params.amount;
+  nft.steward = signerAccount.id;
+  nft.signedAmount = event.params.amount;
   nft.uri = event.params.uri.toString();
   nft.save();
 
-  donorAccount.save();
+  signerAccount.save();
 }
 
 export function handleTransfer(event: Transfer): void {}
