b78b03279329de07cdb88b3f4f3bd87ae23992c9
Angela Gilhotra <angelagilhotra@gmail.com>
2022-05-16 04:30:40 -0300

    fix

--- packages/dapp/src/components/HighlightModal/index.js
+++ packages/dapp/src/components/HighlightModal/index.js
@@ -6,7 +6,7 @@ const Modal = () => {
     <div className="flex justify-center md:p-8 w-full h-full md:h-auto md:w-min my-auto rounded-lg shadow-xl bg-white ">
       <div className="flex flex-col gap-y-8 items-center my-auto">
         <div className="flex flex-col font-redaction justify-between">
-          <Mintable text="hello" handleFinishedDrawing={() => console.log('handle')} />
+          <Mintable />
           <Footer />
         </div>
       </div>
