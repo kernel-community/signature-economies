d33275a3901c266752b4ccb6e191008d266c3240
Angela Gilhotra <angelagilhotra@gmail.com>
2022-05-16 18:31:42 -0400

    generate metadata, upload, mint

--- packages/dapp/src/components/HighlightModal/Footer.js
+++ packages/dapp/src/components/HighlightModal/Footer.js
@@ -1,9 +1,11 @@
 import { useContext } from 'react';
 import { Connector } from "../../utils/connect"
-import { useConnect } from "wagmi"
+import { useConnect, useSigner, useContract } from "wagmi"
 import { HighlightContext } from '../../contexts/Highlight';
 import { upload } from "../../utils/arweave/index";
 import { Buffer } from 'buffer';
+import { generate } from '../../utils/eth/metadata';
+import { addresses, abis } from '../../utils/eth/constants';
 
 const basicButtonStyle = `w-32 px-4 py-2 border-2 border-gray-200 rounded-md hover:border-gray-400 transition-all cursor-pointer`
 const greenButtonStyle = `w-32 px-4 py-2 bg-green-600 shadow shadow-green-300 transition-all hover:shadow-md hover:text-green-50 hover:shadow-green-500 text-green-300 border-2 border-transparent rounded-md cursor-pointer`
@@ -13,7 +15,19 @@ const greenButtonDisabledStyle = `w-32 px-4 py-2 bg-gray-600 shadow shadow-green
 
 const Footer = () => {
   const { state, dispatch } = useContext(HighlightContext);
-  const [{ data, error }, connect] = useConnect()
+  const {
+    activeConnector,
+    connect,
+    connectors,
+    error,
+  } = useConnect();
+  const { data: signer } = useSigner();
+  const signatureNFTContract = useContract({
+    addressOrName: addresses(1337).signatureNFT, // @todo - need a better way to manage this, the chainid from provider will be a promise, and probably can't be used here
+    contractInterface: abis.signatureNFT,
+    signerOrProvider: signer,
+  })
+
   const isImage = !!state.image;
 
   const mint = async () => {
@@ -21,10 +35,23 @@ const Footer = () => {
       // throw an error here or prompt the user to reload
       return;
     }
+
+    // fetch and upload image
     const image = Buffer.from(state.image.split(",")[1], "base64")
-    const { id } = await upload({ data: image, contentType: 'image/png' });
-    console.log('id', id); // url - https://arweave.net/${id} ; or localhost:1984/${id}
-    dispatch({ type: 'metadata', payload: id });
+    const { arUrl: imageUrl } = await upload({ data: image, contentType: 'image/png' });
+
+    // fetch, generate and upload metadata
+    const tokenId = (await signatureNFTContract.tokenIdCounter()).toString();
+    const metadata = generate(tokenId, imageUrl, state.text.length);
+    const { arUrl: metadataUrl } = await upload({
+      data: JSON.stringify(metadata),
+      contentType: 'text/plain'
+    });
+
+    console.log(metadataUrl);
+
+    // mint nft
+    await signatureNFTContract.mintSelected(metadataUrl);
   }
   return (
     <div className="flex flex-row w-full justify-center gap-x-4 text-center my-5">
@@ -34,23 +61,23 @@ const Footer = () => {
     >
       Cancel
     </div>
-    {!data.connected && (
+    {!activeConnector ? (
       <div
         className={greenButtonStyle}
-        onClick={() => connect(data.connectors[Connector.INJECTED])}>
+        onClick={() => connect(connectors[Connector.INJECTED])}>
         {error && error.message && <div>Failed to connect</div>}
         Connect
       </div>
-    )}
-    {data.connected && (
+    ) : (
       <div
-        className={isImage ? greenButtonStyle : greenButtonDisabledStyle}
-        onClick={mint}
-        disabled={!isImage}
+      className={isImage ? greenButtonStyle : greenButtonDisabledStyle}
+      onClick={mint}
+      disabled={!isImage}
       >
         Mint
       </div>
-    )}
+      )
+    }
   </div>
   )
 }
