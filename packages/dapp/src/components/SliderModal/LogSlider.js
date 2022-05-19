23b9af137db14fb415bef956084ced0d42c871e5
Daanish <danishabbir@users.noreply.github.com>
2022-05-19 18:34:06 +0400

    log scale slider
    Math.Exp and Math.Log, and introducing rc-slider dependency for easy rendering of axis markers

---
+++ packages/dapp/src/components/SliderModal/LogSlider.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+
+// following dmt0's example 
+
+// just some cosmetics
+const prettyInt = (x) =>
+  x.toFixed(1)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+// change these to whatever curve function you need!
+export const sliderCurve = Math.exp;
+export const inverseCurve = Math.log;
+const makeMarks = (a) =>
+  a.reduce((acc, cur) => {
+    acc[inverseCurve(cur)] = prettyInt(cur);
+    return acc;
+  }, {});
+
+const LogSlider = ({ min, max, marks, stepsInRange, onChange }) => (
+  <Slider
+    min={inverseCurve(min)}
+    max={inverseCurve(max)}
+    marks={makeMarks(marks)}
+    step={(inverseCurve(max) - inverseCurve(min)) / stepsInRange}
+    tipFormatter={(value) => prettyInt(sliderCurve(value))}
+    onChange={onChange}
+  />
+);
+
+export default LogSlider;
