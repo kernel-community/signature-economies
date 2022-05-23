452932ec78f8f2a83144f76b301e22c38ee99303
malayvasa <malayvasa@gmail.com>
2022-05-23 11:02:50 +0530

    Shift highlighted text inside p5 canvas ✨

--- packages/dapp/src/components/HighlightModal/Mintable.js
+++ packages/dapp/src/components/HighlightModal/Mintable.js
@@ -7,16 +7,7 @@ const Mintable = () => {
   const {state, dispatch} = useContext(HighlightContext);
   return (
     <div className="relative rounded-lg shadow-lg w-auto h-auto" id ="nft">
-    <div className="absolute z-50 text-[#BBBBBB]/80 flex flex-col h-1/3 bottom-0 w-full font-syne text-sm text-left px-4">
-      <div className="flex-grow w-full border-y border-[#BBB]/20 max-h-full flex text-justify items-center">
-        {state.text}
-      </div>
-      <div className="flex flex-row items-center text-[#BBB]/60 justify-between h-12 text-lg">
-        <div>Kernel Verses</div>
-        <div>Signature Economies</div>
-      </div>
-    </div>
-    <div className="absolute w-full h-1/3 bottom-0 blur-sm bg-transparent backdrop-blur-sm"></div>
+
     {/*
       this is a hack since P5 canvas was rendering to blank on each state update
       canvas will only be drawn each time state.image is undefined
