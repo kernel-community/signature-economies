cc598874269eb3e6f7541e0083f170f6d5c5c2c0
Angela Gilhotra <angelagilhotra@gmail.com>
2022-05-27 01:54:44 -0300

    send correct value from slider

--- packages/dapp/src/utils/eth/contracts.js
+++ packages/dapp/src/utils/eth/contracts.js
@@ -2,16 +2,15 @@ import { Contract } from 'ethers'
 import { addresses, abis } from './constants'
 import { ethers } from 'ethers';
 
-const VALUE = '0.1';
-
-export const createSign = async (selected, provider, signer) => {
+export const createSign = async ({ value, token, provider, signer }) => {
   const { chainId } = await provider.getNetwork()
-  const value = ethers.utils.parseEther(VALUE);
   const signatureFundContract = new Contract(
       addresses(chainId).signatureFund,
       abis.signatureFund,
       signer)
-  return signatureFundContract.createSign(selected, {value: value, gasLimit: 200000})
+  return signatureFundContract.createSign(
+    token, { value:  ethers.utils.parseEther(value) }
+  );
 }
 
 export const mintSelected = async (url, provider, signer) => {
