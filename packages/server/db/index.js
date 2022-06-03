9a6676f3aba9abaebd379738dd73bf811258b0f4
Angela Gilhotra <angelagilhotra@gmail.com>
2022-06-03 02:15:18 -0300

    remove db

--- packages/server/db/index.js
+++ packages/server/db/index.js
@@ -1,30 +1,13 @@
-const levelup = require("levelup");
-const leveldown = require("leveldown");
 const { ethers } = require("ethers");
-const path = require("path");
 const { tokenIdContractCheck } = require("../utils");
 
-const dbPath = path.join(__dirname, "tokens");
-
-const tokens = levelup(leveldown(dbPath));
-
 const getRandomTokenId = () => ethers.BigNumber.from(ethers.utils.randomBytes(32)).toString();
-
-// returns true if token id found in db
-const tokenIdDbCheck = async (id) => {
-  let exists = false;
-  try {
-    exists = !!(await tokens.get(id));
-  } catch(err) { /** ignore error here */ }
-  return exists;
-}
 
 exports.getOrStoreRandomId = async (url) => {
   let exists, id;
   do {
     id = getRandomTokenId();
-    exists = (await tokenIdDbCheck(id)) || (await tokenIdContractCheck(id));
+    exists = await tokenIdContractCheck(id);
   } while (exists);
-  await tokens.put (id, url);
   return { id, url }
 }
