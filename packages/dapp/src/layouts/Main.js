bfad9563cc7dee79ee6a957ff436887fae5828bb
Angela Gilhotra <angelagilhotra@gmail.com>
2022-06-09 22:50:19 -0300

    fix layout

--- packages/dapp/src/layouts/Main.js
+++ packages/dapp/src/layouts/Main.js
@@ -1,13 +1,16 @@
 import Footer from "../components/Footer";
+import Header from "../components/Header";
 import HorizontalRule from "../components/common/HorizontalRule";
 
 
 const Main = ({children}) => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
+      <Header />
       <div className="flex-1">
+      <HorizontalRule />
         {children}
-        <HorizontalRule />
+      <HorizontalRule />
       </div>
       <Footer />
     </div>
