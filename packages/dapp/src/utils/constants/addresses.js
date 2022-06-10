914704a9522a226f3338aeeda261cfd298589cf3
Angela Gilhotra <angelagilhotra@gmail.com>
2022-06-10 02:04:21 -0300

    i think? these are the rinkeby addresses?

--- packages/dapp/src/utils/constants/addresses.js
+++ packages/dapp/src/utils/constants/addresses.js
@@ -2,8 +2,8 @@ exports.addresses = (chainId) => {
   switch (chainId) {
     case 4:
       return {
-        signatureFund: '0x6E10CaDF8F623d90b36A88f8F42a4E02221F6BB9',
-        signatureNFT: '0x3488a78A9F5ac75486a1e179447682200DF4D637',
+        signatureFund: '0xcAb3Dc219b3F11dc8Bf27a08400E40Ca0F72096b',
+        signatureNFT: '0x374ca402e33d19598b90de5f3fc64d0c06563ca9',
       }
     case 1337:
       return {
