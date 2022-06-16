e997fd3fc372887e45f3789cb3c1641c77d8fbde
andytudhope <tuddy0525@gmail.com>
2022-06-16 11:39:50 +0200

    Removes my potentially braaking change

--- packages/dapp/src/components/FreeSign/SignatureList.js
+++ packages/dapp/src/components/FreeSign/SignatureList.js
@@ -1,23 +1,18 @@
-import { useNetwork } from "wagmi";
-
 const Constants = require('../../utils/constants');
 
 const {protocol, host, port} = Constants.arweave.gateway;
 const arweaveUrl = protocol + "://" + host + ":" + port;
 
-const trimAddress = (address) => address.substring(0,8).concat("..."); 
+const trimAddress = (address) => address.substring(0,8).concat("...");
+const ETHERSCAN_ACCOUNT = "https://etherscan.com/address";
 
 const AccountAddress = ({ account, ens }) => {
   const toDisplay = ens ? ens : account ? trimAddress(account) : "Anonymous";
-  const Etherscan = () => {
-    const { activeChain } = useNetwork();
-    return `https://${activeChain.name}etherscan.com/address`;
-  }
 
   return (
       account ? (
         <a
-          href={Etherscan + '/' + account}
+          href={ETHERSCAN_ACCOUNT + '/' + account}
           target="_blank" rel='noreferrer'
           className='no-underline text-gray-500 hover:text-black'
         >
