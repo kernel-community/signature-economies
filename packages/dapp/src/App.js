5b107baf202552a2bc9e6a957d0eab47700ec04d
andytudhope <tuddy0525@gmail.com>
2022-06-17 21:47:19 +0200

    Adds react-helmet for more elegant metadata

--- packages/dapp/src/App.js
+++ packages/dapp/src/App.js
@@ -1,13 +1,12 @@
-import { useContext } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Essay from './pages/Essay'
 import SignedOn from './pages/SignedOn'
 import SoulSigns from './pages/SoulSigns'
 import SwitchNetworkModal from './components/SwitchNetwork'
+import PauseForLoadingModal from './components/PauseForLoadingModal'
 import { SwitchNetworkProvider } from './contexts/SwitchNetwork'
 import { PauseForLoadingProvider } from './contexts/PauseForLoading'
-import PauseForLoadingModal from './components/PauseForLoadingModal'
 
 const App = () => {
   return (
--- packages/dapp/src/layouts/Main.js
+++ packages/dapp/src/layouts/Main.js
@@ -1,18 +1,23 @@
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import HorizontalRule from '../components/common/HorizontalRule'
+import MetaData from '../components/Metadata'
 
 const Main = ({ children }) => {
   return (
-    <div className='min-h-screen flex flex-col items-center justify-center'>
-      <Header />
-        <div className='flex-1 w-full'>
-          <HorizontalRule />
-          {children}
-          <HorizontalRule />
-        </div>
-      <Footer />
-    </div>
+    <>
+      <MetaData />
+      <div className='min-h-screen flex flex-col items-center justify-center'>
+        <Header />
+          <div className='flex-1 w-full'>
+            <HorizontalRule />
+            {children}
+            <HorizontalRule />
+          </div>
+        <Footer />
+      </div>
+    </>
+    
   )
 }
 
