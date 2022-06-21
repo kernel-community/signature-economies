0fb34bfe67013aeff7743baa14c2bb76e78df484
Angela Gilhotra <angelagilhotra@gmail.com>
2022-06-21 19:03:59 -0300

    fix etherscan link

--- packages/dapp/src/utils/constants/index.js
+++ packages/dapp/src/utils/constants/index.js
@@ -34,7 +34,7 @@ export const etherscan = {
     switch (id) {
       case 4: return 'https://rinkeby.etherscan.io'
       case 1: return 'https://etherscan.io'
-      default: return 'https://rinkeby.etherscan.io'
+      default: return 'https://etherscan.io'
     }
   }
 }
